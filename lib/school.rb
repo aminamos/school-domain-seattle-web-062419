7c4ce48b20ef3971db86c8704b6bde036ab34769
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-15 10:27:52 -0700

    draft

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    # @@roster = {}
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+        roster = @roster
+    end
+
+    def roster
+        @roster
+    end
+
+    def @roster.[]=(key, value)
+        @roster[key] = value
+    end
+    
+    def add_student(name, grade)
+        @roster.map {|key1,value1|
+            if @roster.has_key?(grade)
+                @roster[grade] << name
+            else
+                @roster[grade] = []
+                @roster[grade] << name
+            end
+        }
+    end
+end
+
+binding.pry
